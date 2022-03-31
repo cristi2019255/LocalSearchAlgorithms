3cc89c8432953aa40dbbefa46f401ae7ba235ef4
Cristian Grosu <cristian@codinglab.com>
2022-03-31 16:11:40 +0200

    automating experiments

--- main.py
+++ main.py
@@ -6,46 +6,30 @@ from local_search_algorithms.FM import FM_pass
 from local_search_algorithms.GLS import GLS
 from local_search_algorithms.ILS import ILS, adaptive_ILS
 from local_search_algorithms.MLS import MLS
-from utils import compute_statistics, experiment, experiments_delta_time, stop_after_calls
+from utils import RESULTS_DIRECTORY, compute_statistics, experiment, experiments_delta_time, experiments_stop_after_calls, stop_after_calls
 
 
 FILE_NAME = './G500.txt'
 
 def test_FM(graph, positions):        
     start = datetime.now()
-    solution, cuts = GLS(graph = graph, nr_of_calls=10000, population_size=100)
+    solution, cuts = GLS(graph = graph, stopping_criterion=stop_after_calls, population_size=100)
     end = datetime.now()
     print('Min cuts: ' + str(cuts))
     print('Run time: ' + str(end-start))    
     plot_partitioned_graph(graph = graph, partition= solution, positions=positions)    
 
+
 def main():
-    graph, positions = read_graph_from_file(filename=FILE_NAME)                    
+    graph, positions = read_graph_from_file(filename=FILE_NAME)  
+                      
     #graph, positions = generate_graph(500,random=True)                       
     #plot_graph(graph, positions)       
     #test_FM(graph, positions)
-    #experiment('./Results/MLS.txt', MLS)(graph = graph, stopping_criterion = stop_after_calls)
     
-    #experiment('./Results/ILS_01.txt', ILS)(graph = graph, probability = 0.01 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/ILS_05.txt', ILS)(graph = graph, probability = 0.05 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/ILS_10_experimental_1.txt', ILS)(graph = graph, probability = 0.1 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/ILS_15.txt', ILS)(graph = graph, probability = 0.15 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/ILS_20.txt', ILS)(graph = graph, probability = 0.2 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/ILS_50.txt', ILS)(graph = graph, probability = 0.5 , stopping_criterion = stop_after_calls)
-    #experiment('./Results/adaptive_ILS.txt', adaptive_ILS)(graph = graph, stopping_criterion = stop_after_calls, P_min = 0.1, alpha = 0.5, beta = 0.5, operators = [0.01, 0.05, 0.1, 0.15, 0.2, 0.5])
+    experiments_stop_after_calls(graph)    
     
-    #experiment('./Results/GLS_5.txt', GLS)(graph = graph, population_size = 5 , stopping_criterion = stop_after_calls)   
-    #experiment('./Results/GLS_10.txt', GLS)(graph = graph, population_size = 10, stopping_criterion = stop_after_calls)
-    #experiment('./Results/GLS_20.txt', GLS)(graph = graph, population_size = 20, stopping_criterion = stop_after_calls)
-    experiment('./Results/GLS_50.txt', GLS)(graph = graph, population_size = 50, stopping_criterion = stop_after_calls)
-    #experiment('./Results/GLS_100.txt', GLS)(graph = graph, population_size = 100, stopping_criterion = stop_after_calls)
-    #experiment('./Results/GLS_300.txt', GLS)(graph = graph, population_size = 300, stopping_criterion = stop_after_calls)
-    #experiment('./Results/GLS_500.txt', GLS)(graph = graph, population_size = 500, stopping_criterion = stop_after_calls)
-    
-    
-    #experiments_delta_time(graph) 
-    
-    #compute_statistics()
+    #experiments_delta_time(graph)         
     
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -2,20 +2,20 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from time import time
+import numpy as np
 
 from local_search_algorithms.FM import FM_pass
 from local_search_algorithms.GLS import GLS
-from local_search_algorithms.ILS import ILS
+from local_search_algorithms.ILS import ILS, adaptive_ILS
 from local_search_algorithms.MLS import MLS
 
 NR_OF_RUNS = 25  
 NR_OF_FM_CALLS = 10000
-RESULTS_DIRECTORY = './Results' 
-#EXPERIMENTS = ['MLS','ILS_01', 'ILS_05', 'ILS_10', 'ILS_15', 'ILS_20', 'ILS_50','adaptive_ILS', 'GLS_5', 'GLS_10','GLS_20', 'GLS_50', 'GLS_100', 'GLS_300', 'GLS_500']
+RESULTS_DIRECTORY = './Results/' 
 EXPERIMENTS = ['MLS', 'ILS_15', 'adaptive_ILS','GLS_50']
     
-def compute_statistics():            
-    file_names = list(map(lambda x: './Results/' + x + '.txt', EXPERIMENTS))
+def compute_statistics(title = 'comparison of local search algorithms', x_label = 'local searcher', y_label = 'optimum cut', experiments = EXPERIMENTS):
+    file_names = list(map(lambda x: RESULTS_DIRECTORY + x + '.txt', experiments))
     experimental_data = []
     for file_name in file_names:
         with open(file_name, 'r') as file:
@@ -24,28 +24,39 @@ def compute_statistics():
             file.close()    
         experimental_data.append(optimums)
     
+    
+    fig = plt.figure()
     plt.title('Comparison of local search algorithms')
-    plt.boxplot(experimental_data, labels=EXPERIMENTS)    
+    plt.boxplot(experimental_data, labels=experiments)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)    
+    fig_name = './Results/' + title + '.png'
+    fig.savefig(fig_name, dpi=fig.dpi)
     plt.show()
 
 def experiment(results_file_name, func):
     def experiment_wrapper(*args, **kwargs):                
         same_region_proportions = []
+        optimum_cuts = []
         with open(results_file_name, 'w') as file:
             start = datetime.now()
             for _ in tqdm(range(NR_OF_RUNS)): 
                 if func.__name__ == 'ILS':
-                    solution, optimum_cuts, same_region_proportion = func(*args, **kwargs)
+                    _ , optimum_cut, same_region_proportion = func(*args, **kwargs)
                     same_region_proportions.append(same_region_proportion)
                 else:               
-                    solution, optimum_cuts = func(*args, **kwargs)            
-                file.write(str(optimum_cuts) + ' ')
-                                      
+                    _, optimum_cut = func(*args, **kwargs)            
+
+                optimum_cuts.append(optimum_cut)                                                                          
             end = datetime.now()
+            
             print('Run time: ' + str(end-start))                     
+            file.write('Optimum cuts: ' + str(optimum_cuts))                            
+            file.write('\nAverage optimum cut: ' + str(np.mean(optimum_cuts)) + '( ' + str(np.std(optimum_cuts)) + ' )')
             file.write('\nRun time:' + str(end-start))
             if func.__name__ == 'ILS':
                 file.write('\nSame region proportion: ' + str(same_region_proportions))
+                file.write('\nAverage same region proportion: ' + np.mean(same_region_proportions) + '( ' + str(np.std(same_region_proportions)) + ' )')
             file.close()
         print(f'Done, take a look in {results_file_name} ...')
     
@@ -90,10 +101,42 @@ def experiments_delta_time(graph, ils_probability = 0.15, gls_population_size = 
         delta_times.append(delta_time)
         
     ### saving the results
-    with open('./Results/delta_time.txt', 'w') as file:
+    with open( (RESULTS_DIRECTORY + 'delta_time.txt'), 'w') as file:
         file.write('ILS probability: ' + str(ils_probability) + ', GLS population size: ' + str(gls_population_size) + '\n')
         file.write('Delta time: ' + str(delta_times) + '\n')
         file.write(str(mls_optimums) + '\n')
         file.write(str(ils_optimums) + '\n')
-        file.write(str(gls_optimums) + '\n\n')
+        file.write(str(gls_optimums) + '\n\n')        
+        file.write('\nAverage delta time: ' + str(np.mean(delta_times)) + '( ' + str(np.std(delta_times)) + ' )')
+        file.write('\nAverage mls optimum: ' + str(np.mean(mls_optimums)) + '( ' + str(np.std(mls_optimums)) + ' )')
+        file.write('\nAverage ils optimum: ' + str(np.mean(ils_optimums)) + '( ' + str(np.std(ils_optimums)) + ' )')
+        file.write('\nAverage gls optimum: ' + str(np.mean(gls_optimums)) + '( ' + str(np.std(gls_optimums)) + ' )')
         file.close()
+    
+    compute_statistics(title = 'Comparison of local searchers with fixed delta_time', x_label = 'local searcher', y_label = 'Optimum cuts', experiments = ['MLS', 'ILS_' + str(ils_probability), 'GLS'])
+    fig = plt.figure()
+    plt.title('Delta time')
+    plt.plot(delta_times)
+    plt.xlabel('Run')
+    plt.ylabel('Delta time')    
+    fig_name = RESULTS_DIRECTORY + 'Delta_times' + '.png'
+    fig.savefig(fig_name, dpi=fig.dpi)
+    plt.show()
+
+def experiments_stop_after_calls(graph):
+    experiment(RESULTS_DIRECTORY + 'MLS.txt', MLS)(graph = graph, stopping_criterion = stop_after_calls)
+    
+    # experiments ILS
+    experiment(RESULTS_DIRECTORY + 'ILS_004.txt', ILS)(graph = graph, probability = 0.004 , stopping_criterion = stop_after_calls)
+    experiment(RESULTS_DIRECTORY + 'ILS_05.txt', ILS)(graph = graph, probability = 0.05 , stopping_criterion = stop_after_calls)
+    experiment(RESULTS_DIRECTORY + 'ILS_10.txt', ILS)(graph = graph, probability = 0.1 , stopping_criterion = stop_after_calls)
+    experiment(RESULTS_DIRECTORY + 'ILS_15.txt', ILS)(graph = graph, probability = 0.15 , stopping_criterion = stop_after_calls)
+    experiment(RESULTS_DIRECTORY + 'ILS_20.txt', ILS)(graph = graph, probability = 0.2 , stopping_criterion = stop_after_calls)
+    experiment(RESULTS_DIRECTORY + 'ILS_50.txt', ILS)(graph = graph, probability = 0.5 , stopping_criterion = stop_after_calls)
+    
+    experiment(RESULTS_DIRECTORY + 'adaptive_ILS.txt', adaptive_ILS)(graph = graph, stopping_criterion = stop_after_calls, P_min = 0.1, alpha = 0.5, beta = 0.5, operators = [0.004, 0.05, 0.1, 0.15, 0.2, 0.5])
+        
+    experiment(RESULTS_DIRECTORY + 'GLS_50.txt', GLS)(graph = graph, population_size = 50, stopping_criterion = stop_after_calls)
+    
+    compute_statistics(title = 'Comparison of iterative local searchers with fixed number of FM_pass calls', x_label = 'local searcher', y_label = 'Optimum cuts', experiments = ['MLS', 'ILS_004', 'ILS_05', 'ILS_10', 'ILS_15', 'ILS_20', 'ILS_50'])
+    compute_statistics(title = 'Comparison of local searchers with fixed number of FM_pass calls', x_label = 'local searcher', y_label = 'Optimum cuts', experiments = ['MLS', 'ILS_15', 'GLS_50'])
